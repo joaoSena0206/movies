2c1f70944b812619d0cc29d2870e78e9e82d1695
João Sena <joaosena0206@gmail.com>
2025-01-14 15:14:51 -0300

    feat: esconde o icone de pesquisa ao focar na barra de tarefas

--- front/src/js/script.js
+++ front/src/js/script.js
@@ -1,11 +1,23 @@
 $(function () {
-    let menu = $("#mobileMenu");
+    const menu = $("#mobileMenu");
+    const searchBar = $("#searchBar");
+    const bookmark = $("#bookmark");
 
+    // Adiciona animação ao menu quando clicar ou fechar
     $("#menuSpan").on("click", function () {
         menu.animate({ width: "toggle" }, 250);
     });
 
     $("#menuSpanClose").on("click", function () {
         menu.animate({ width: "toggle" }, 250);
+    });
+
+    // Esconde o ícone de pesquisa ao focar na barra de pesquisa
+    searchBar.on("focus", function () {
+        $("#searchIcon").fadeOut(350);
+    });
+
+    searchBar.on("focus", function () {
+        $("#searchIcon").fadeOut(350);
     });
 });
