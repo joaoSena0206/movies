8c98512b067d7a6a0c3b1b7cc23514146273ce4f
João Sena <joaosena0206@gmail.com>
2025-01-17 14:48:38 -0300

    style: adiciona bookmark ao script geral

--- front/src/js/geral.js
+++ front/src/js/geral.js
@@ -2,6 +2,8 @@ $(function () {
     const menu = $("#mobileMenu");
     const searchBar = $("#searchBar");
     const blackBg = $("#blackBg");
+    const bookmark = $(".bookmark");
+    const banner = $(".banner");
 
     // Adiciona animação ao menu quando clicar ou fechar e mostra o fundo preto
     $("#menuSpan").on("click", function () {
@@ -23,5 +25,28 @@ $(function () {
 
     searchBar.on("focusout", function () {
         $("#searchIcon").fadeIn(200);
+    });
+
+    // Adiciona efeito hover no ícone de bookmark
+    bookmark.on("mouseenter", function () {
+        const path = this.getSVGDocument().children[0].children[0];
+
+        path.style.transition = "fill 0.2s ease-in-out";
+        path.style.cursor = "pointer";
+        path.setAttribute("fill", "#434342");
+    });
+
+    bookmark.on("mouseleave", function () {
+        this.getSVGDocument().children[0].children[0].setAttribute("fill", "#1A1A1A");
+    });
+
+    // Adiciona efeito hover no banner do carrosel
+    banner.on("mouseenter", function () {
+        this.style.transition = "opacity 0.2s ease-in-out";
+        this.style.opacity = "90%";
+    });
+
+    banner.on("mouseleave", function () {
+        this.style.opacity = "100%";
     });
 });
