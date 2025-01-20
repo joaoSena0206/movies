52b8f9616df24d4b6f3b0d5ff5b16a54ffc37ef9
João Sena <joaosena0206@gmail.com>
2025-01-20 14:35:19 -0300

    feat: adiciona estrutura inicial do server

---
+++ movies/Controllers/ObraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace movies.Controllers
+{
+    public class ObraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
