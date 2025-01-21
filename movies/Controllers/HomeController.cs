d9e40e9c43763fa5676ae2502c1e9c22e40662e0
João Sena <joaosena0206@gmail.com>
2025-01-21 16:04:04 -0300

    refactor: remove declarações inúteis

--- movies/Controllers/HomeController.cs
+++ movies/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using movies.Models;
 
 namespace movies.Controllers
 {
@@ -21,12 +20,6 @@ namespace movies.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
