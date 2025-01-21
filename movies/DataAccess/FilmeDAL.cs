68b785b7f34a109bfe125cdf6a474a2fe8781279
João Sena <joaosena0206@gmail.com>
2025-01-21 16:03:37 -0300

    feat: adiciona método para se obter todos os filmes

--- movies/DataAccess/FilmeDAL.cs
+++ movies/DataAccess/FilmeDAL.cs
@@ -9,4 +9,35 @@ public class FilmeDAL
     {
         _banco = banco;
     }
+
+    public List<Filme> ObterTodos()
+    {
+        string comando = @"
+        SELECT 
+	        cd_filme,
+	        qt_duracao_filme,
+	        qt_avaliacao_filme,
+	        nm_filme
+        FROM filme;
+        ";
+
+        using (SqlDataReader reader = _banco.ExecutarConsulta(comando))
+        {
+            List<Filme> filmes = new List<Filme>();
+
+            while (reader.Read())
+            {
+                Filme filme = new Filme();
+                filme.Codigo = reader.GetInt32(0);
+                filme.Duracao = reader.GetTimeSpan(1);
+                filme.Avaliacao = reader.GetDecimal(2);
+                filme.Nome = reader.GetString(3);
+
+                filmes.Add(filme);
+            }
+
+
+            return filmes;
+        }
+    }
 }
