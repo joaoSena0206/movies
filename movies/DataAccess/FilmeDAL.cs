92fee37903aacab27d47d0a3e87675be3040312f
João Sena <joaosena0206@gmail.com>
2025-01-21 16:03:17 -0300

    chore: adiciona estrutura inicial

---
+++ movies/DataAccess/FilmeDAL.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+using movies.Data;
+
+public class FilmeDAL
+{
+    private readonly Banco _banco;
+
+    public FilmeDAL(Banco banco)
+    {
+        _banco = banco;
+    }
+}
