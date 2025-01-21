a9271958d3a452f30e0d7c34f4fe5681ca7a7e77
João Sena <joaosena0206@gmail.com>
2025-01-21 15:43:57 -0300

    feat: adiciona classe Banco para centralizar a conexão ao banco de dados

---
+++ movies/DataAccess/Banco.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace movies.Data
+{
+    public class Banco
+    {
+        private readonly string _connectionString;
+
+        public Banco(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public void ExecutarComando(string comando, List<SqlParameter> parametros)
+        {
+            using (SqlConnection connection = new SqlConnection(this._connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(comando, connection);
+
+                if (parametros != null)
+                {
+                    command.Parameters.AddRange(parametros.ToArray());
+                }
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public SqlDataReader ExecutarConsulta(string comando, List<SqlParameter> parametros)
+        {
+            using (SqlConnection connection = new SqlConnection(this._connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(comando, connection);
+
+                if (parametros != null)
+                {
+                    command.Parameters.AddRange(parametros.ToArray());
+                }
+
+                return command.ExecuteReader();
+            }
+        }
+    }
+}
